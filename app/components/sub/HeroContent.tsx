764e3533d5f783323146eaa19eda62dc6ac5b39e
SAIYAM RAWAT <139794457+saiyam0905@users.noreply.github.com>
2024-07-03 11:34:07 +0530

    Update HeroContent.tsx

--- app/components/sub/HeroContent.tsx
+++ app/components/sub/HeroContent.tsx
@@ -51,7 +51,7 @@ const HeroContent = () => {
         <motion.a
         variants={slideInFromLeft(1)}
         className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'
-         href='https://tinyurl.com/saiyam-resume' > 
+         href='https://drive.google.com/file/d/1FhinQGvo_ai9RsEwxGJb1vwifSRGpAIC/view?usp=sharing' > 
         Resume   
 
         </motion.a>
